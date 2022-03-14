5783f9eccf7e08e0179d4b9ac83b6a243f66f5fc
Vikram Negi <viknegi0@gmail.com>
2022-03-15 00:42:43 +0530

    python script to extract words

---
+++ api/script.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+
+while True:
+  try:
+    word_len = int(input("Enter the word length: "))
+    break
+  except:
+    print("Please enter a valid number!\n")
+    continue
+
+path = os.getcwd()
+
+save_file_name = f"/{word_len}_letter_words.txt"
+
+with open(path + "/words_alpha.txt", "r") as main_file, open(path + save_file_name, "w+") as save_file:
+
+  print(f"\nSaving all {word_len} letter words to a file...")
+
+  word_count = 0
+
+  for word in main_file:
+    word = word.strip()
+
+    if word_len == len(word):
+      word_count += 1
+      save_file.write(word + "\n")
+
+print(f"\n{word_count} word{'' if word_count == 1 else 's'} saved in file {save_file_name[1:]} 🥳")
+
