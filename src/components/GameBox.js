2da064be76e9ba75108740c124c8116fa1bb4dac
Vikram Negi <viknegi0@gmail.com>
2022-03-18 15:34:39 +0530

    word length limited to numLetter

--- src/components/GameBox.js
+++ src/components/GameBox.js
@@ -1,20 +1,19 @@
 import React, { useEffect, useState } from "react";
 
-// Each box gets mapped into a div
-const GuessBoxes = (props) => {
-  return Array.from(Array(props.numLetters).keys())
+// The guess-row, we have 6 of them!
+const GuessRow = (props) => {
+  // All boxes get mapped into a div-row
+  const boxes = Array
+    .from(Array(props.numLetters).keys())
     .map((_, i) => {
       return (
         <div className="box" key={i}></div>
       );
     });
-}
 
-// The guess-row, we have 6 of them!
-const GuessRow = (props) => {
   return (
     <div className="game-row">
-      <GuessBoxes numLetters={props.numLetters} />
+      { boxes }
     </div>
   );
 }
@@ -33,24 +32,35 @@ export default function GameBox(props) {
     i++;
   }
 
-  const [ allGuesses, setAllGuesses ] = useState(guesses);
+  // const [ allGuesses, setAllGuesses ] = useState(guesses);
+  // console.log(allGuesses);
 
-  console.log(allGuesses);
-
-  const makeWord = (event) => {
-    // console.log(event);
-    // only alphabets allowed
-    if (event.keyCode >= 65 && event.keyCode <= 90) {
-      setWord((prevWord) => prevWord.concat(event.key.toLowerCase()));
-    } else if (event.keyCode === 8 || event.keyCode === 46) {
-      // console.log("backspace");
-      setWord(prevWord => prevWord.slice(0, -1));
-    } else if (event.keyCode === 13) {
-      console.log("enter!");
-    }
-  }
+  
 
   useEffect(() => {
+
+    const makeWord = (event) => {
+      // only alphabets allowed
+      if (event.keyCode >= 65 && event.keyCode <= 90) {
+        setWord((prevWord) => {
+          // limited to numLetters length
+          if (prevWord.length === props.numLetters) {
+            console.log("reached the word length limit!");
+            return prevWord;
+          } else {
+            return prevWord.concat(event.key.toLowerCase())
+          }
+        });
+      }
+      else if (event.keyCode === 8 || event.keyCode === 46) {
+        setWord(prevWord => prevWord.slice(0, -1));
+      }
+      else if (event.keyCode === 13) {
+        console.log("enter!");
+        console.log(word, word.length);
+      }
+    }
+
     document.addEventListener("keyup", makeWord);
 
     return () => {
@@ -59,7 +69,7 @@ export default function GameBox(props) {
     }
   }, []);
 
-  word && console.log("word:", word);
+  word && console.log(word, word.length);
 
   const allGuessRows = guesses.map((_, i) => {
     return (
@@ -69,7 +79,7 @@ export default function GameBox(props) {
 
   return (
     <div className="game-box">
-      {allGuessRows}
+      { allGuessRows }
     </div>
   );
 }
