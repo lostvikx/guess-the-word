816daed0a02ce3855442f9cfc17dfa2af0d44184
Vikram Negi <viknegi0@gmail.com>
2022-03-18 17:08:19 +0530

    refactor

--- src/components/GameBox.js
+++ src/components/GameBox.js
@@ -6,8 +6,9 @@ const GuessRow = (props) => {
   const boxes = Array
     .from(Array(props.numLetters).keys())
     .map((_, i) => {
+      const letter = props.word[i];
       return (
-        <div className="box" key={i}></div>
+        <div className="box" key={i}>{letter}</div>
       );
     });
 
@@ -22,7 +23,7 @@ export default function GameBox(props) {
 
   // This can be changed to alter the difficulty of the game!
   const numOfGuesses = 6;
-  const [word, setWord] = useState([]);
+  const [ word, setWord ] = useState([]);
   // const [metaWord, setMetaWord] = useState("vikram");
 
   const guesses = [];
@@ -32,16 +33,18 @@ export default function GameBox(props) {
     i++;
   }
 
-  // const [ allGuesses, setAllGuesses ] = useState(guesses);
+  const [ allGuesses, setAllGuesses ] = useState(guesses);
   // console.log(allGuesses);
 
-  
-
   useEffect(() => {
-
     const makeWord = (event) => {
+
+      const keyIsChar = event.keyCode >= 65 && event.keyCode <= 90;
+      const keyIsDel = event.keyCode === 8 || event.keyCode === 46;
+      const keyIsEnter = event.keyCode === 13;
+
       // only alphabets allowed
-      if (event.keyCode >= 65 && event.keyCode <= 90) {
+      if (keyIsChar) {
         setWord((prevWord) => {
           // limited to numLetters length
           if (prevWord.length === props.numLetters) {
@@ -52,34 +55,56 @@ export default function GameBox(props) {
           }
         });
       }
-      else if (event.keyCode === 8 || event.keyCode === 46) {
+      else if (keyIsDel) {
         setWord(prevWord => prevWord.slice(0, -1));
       }
-      else if (event.keyCode === 13) {
+      else if (keyIsEnter) {
         console.log("enter!");
-        console.log(word, word.length);
+        if (word.length === props.numLetters) {
+          setAllGuesses(word);
+        }
       }
     }
 
     document.addEventListener("keyup", makeWord);
 
+    // Clean-up function
     return () => {
-      console.log("cleaning up...")
+      // console.log("cleaning up...");
       document.removeEventListener("keyup", makeWord);
     }
-  }, []);
+  }, [ word ]);
 
   word && console.log(word, word.length);
+  console.log(allGuesses);
 
   const allGuessRows = guesses.map((_, i) => {
     return (
-      <GuessRow numLetters={props.numLetters} key={i} />
+      <GuessRow 
+        numLetters={props.numLetters}
+        word={word}
+        key={i}
+      />
     );
   });
 
+  const testWords = ["vikram", "yellow"];
+
   return (
     <div className="game-box">
-      { allGuessRows }
+      {/* { allGuessRows } */}
+      <GuessRow
+        numLetters={props.numLetters}
+        word={testWords[0]}
+      />
+      <GuessRow
+        numLetters={props.numLetters}
+        word={testWords[1]}
+      />
+      <GuessRow
+        numLetters={props.numLetters}
+        word={word}
+      />
     </div>
   );
 }
